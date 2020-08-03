9d282551ff1d86016a6f89510d8e0b6b845c9942
SecBack <albert3257@live.dk>
2020-08-03 12:03:14 +0200

    async angle update working

--- main.cpp
+++ main.cpp
@@ -2,19 +2,20 @@
 #include "stm32746g_discovery_lcd.h"
 #include "stm32746g_discovery_ts.h"
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE 500ms
-bool buttonStatus = false;
+int buttonStatus = 0;
+int angle;
 
+InterruptIn button(D2);
 DigitalOut led(D3); // Initialise the digital pin LED1 as an output
+AnalogIn rotary(A0);
+
+
 
 void buttonSwitch() {
-    if (buttonStatus == false) {
-        led.write(1);
-        buttonStatus = true;
+    if (buttonStatus == 0) {
+        buttonStatus = 1;
     } else {
-        led.write(0);
-        buttonStatus = false;
+        buttonStatus = 0;
     }
 }
 
@@ -25,42 +26,71 @@ void lcdStart()
     BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
 
     BSP_LCD_SetFont(&Font12);
+    BSP_LCD_Clear(uint32_t LCD_COLOR_GREEN);
     BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
     BSP_LCD_SetBackColor(LCD_COLOR_GREEN);
 
     BSP_LCD_DisplayStringAt(0, LINE(3), (uint8_t *)"Advanced Case", CENTER_MODE);
     BSP_LCD_DisplayStringAt(0, LINE(4), (uint8_t *)"31/7/2020", CENTER_MODE);
     BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"By Albert", CENTER_MODE);
-    HAL_Delay(4000);
-    BSP_LCD_cLEAR
+    HAL_Delay(3000);
+    BSP_LCD_Clear(uint32_t LCD_COLOR_BLUE);
 }
 
 void lcdDraw()
 {
+    char text[30];
+    if (angle < 10) {
+        sprintf(text, "angle:    %i", (int)angle);
+    } else if (angle < 100) {
+        sprintf(text, "angle:   %i", (int)angle);
+    } else if (angle < 1000) {
+        sprintf(text, "angle:  %i", (int)angle);
+    } else {
+        sprintf(text, "angle: %i", (int)angle);
+    }
+
+    char buttonText[30];
+    sprintf(buttonText, "Status: %i", buttonStatus);
+    
     BSP_LCD_SetFont(&Font12);
     BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
     BSP_LCD_SetBackColor(LCD_COLOR_BLUE);
+    BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *) &text, CENTER_MODE);
+    BSP_LCD_DisplayStringAt(0, LINE(6), (uint8_t *) &buttonText, CENTER_MODE);
+    HAL_Delay(50);
+    BSP_LCD_ClearStringLine(5);
+    BSP_LCD_ClearStringLine(6);
+}
 
-    BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"insert info here", CENTER_MODE);    
+void blink() 
+{
+    while(true){
+        if (buttonStatus == true) {
+            led = !led;
+            ThisThread::sleep_for(std::chrono::milliseconds(angle));
+            printf ("angle: %d \n", angle);
+        }
+    }
 }
 
 int main()
 {
-    Thread LCD_control;
     lcdStart();
-
-    InterruptIn button(D2);
     button.rise(&buttonSwitch);
-    AnalogIn rotary(A0);
-    float angle;
+
+    Thread ledController;
+    ledController.start(&blink);
 
     while(true) {
-        while (buttonStatus == true) {
-            angle = rotary.read() * 1024;
-            printf ("angle: %d \n", int(angle));
-            led = !led;
-            wait_us(angle*1000);
+        angle = (int)(rotary.read() * 1024);
+
+        if (buttonStatus == 0) {
+            led.write(0);
         }
+            
+        Thread LCD_control;
         LCD_control.start(&lcdDraw);
+        ThisThread::sleep_for(15ms);
     }
 }
